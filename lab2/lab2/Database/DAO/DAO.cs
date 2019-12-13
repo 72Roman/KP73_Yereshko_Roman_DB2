49a5b42d5281a338f9c35a9e4c6e18cf4c705811
72Roman <27romario@gmail.com>
2019-12-13 23:06:24 +0200

    added DAO and DbConnection

---
+++ lab2/lab2/Database/DAO/DAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2.Database.DAO
+{
+    public abstract class Dao<T>
+    {
+        protected DbConnection Dbconnection;
+        protected Dao(DbConnection db)
+        {
+            Dbconnection = db;
+        }
+
+        public abstract void Create(T entity);
+        public abstract T Get(long id);
+        public abstract List<T> Get(int page);
+        public abstract void Update(T entity);
+        public abstract void Delete(long id);
+        public abstract void Clear();
+    }
+}
--- lab2/lab2/Models/Chat.cs
+++ lab2/lab2/Models/Chat.cs
@@ -6,5 +6,17 @@ namespace lab2.Models
 {
     class Chat
     {
+        public long Id { get; set; }
+        public string Tag { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        public Chat(long id, string tag = null, string name = null, string bio = null)
+        {
+            Id = id;
+            Tag = tag;
+            Name = name;
+            Bio = bio;
+        }
     }
 }
