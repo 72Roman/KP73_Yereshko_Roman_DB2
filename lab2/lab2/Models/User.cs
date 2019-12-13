6053db9ed3c38399b6eee5902038da121d717021
72Roman <27romario@gmail.com>
2019-12-13 22:55:19 +0200

    user model created

--- lab2/lab2/Models/User.cs
+++ lab2/lab2/Models/User.cs
@@ -6,5 +6,17 @@ namespace lab2.Models
 {
     class User
     {
+        public long Id { get; set; }
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        public User(long id, string login = null, string name = null, string bio = null)
+        {
+            Id = id;
+            Login = login;
+            Name = name;
+            Bio = bio;
+        }
     }
 }
